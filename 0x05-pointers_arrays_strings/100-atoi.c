101fecd85c4762a459e390cb9ca361db9db302c2
Imad-EL-IDRISSI <iei.2002.imad@gmail.com>
2023-06-26 17:11:51 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -19,6 +19,7 @@ int _atoi(char *s)
 
 	while (s[len] != '\0')
 		len++;
+
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
@@ -37,7 +38,8 @@ int _atoi(char *s)
 		}
 		i++;
 	}
-        if (f == 0)
+
+	if (f == 0)
 		return (0);
 
 	return (n);
