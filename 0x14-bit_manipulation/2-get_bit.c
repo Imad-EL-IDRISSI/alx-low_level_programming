f1f0e05a1cfb16f672073be178ed72da67a05280
Imad-EL-IDRISSI <iei.2002.imad@gmail.com>
2023-08-04 08:02:15 -0700

    updated

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_bit -function that returns the value of a bit at an index in a decimal number
+ * get_bit - returns the value of a bit at an index in a decimal number
  * @n: number to search
  * @i: index of the bit
  *
