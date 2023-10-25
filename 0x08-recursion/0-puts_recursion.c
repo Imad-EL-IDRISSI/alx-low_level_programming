950307fea2c0d8efb550b7aec8d454adf1545c0d
EL-IDRISSI-Imad <iei.2002.imad@gmail.com>
2023-10-25 13:07:21 -0700

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,12 +5,12 @@
  * @*s is the pointer of the char s
  * Return : void
  */
-void _puts_recursion(char *s){
+void _puts_recursion(char *s)
+{
 	if (*s == '\0'){
 		return;
 	} else{
 		_putchar(*s);
-
 		_puts_recursion(s+1);
 	}
 }
