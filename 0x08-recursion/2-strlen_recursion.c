99ec0e813b8e82bc66ab084eff3ac2982dc518f7
EL-IDRISSI-Imad <iei.2002.imad@gmail.com>
2023-10-25 13:51:14 -0700

    update

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,13 +3,14 @@
 /**
  * _strlen_recursion - function that return the length of the string given
  * @s : the string concerned
- * Return : 0 if the string is empty
+ *
+ * Return : the length of the string
  */
 
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return 0;
-
-	return 1 +_strlen_recursion(s + 1);
+		return (0); /* Base case: End of the string, length = 0 */
+	/* Recursive call of the function with the next character and add 1 */
+	return 1 + _strlen_recursion(s + 1);
 }
