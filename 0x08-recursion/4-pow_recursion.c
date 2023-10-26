a569718bed5a49669c4865d9f9644516a4e1e329
EL-IDRISSI-Imad <iei.2002.imad@gmail.com>
2023-10-26 01:57:25 -0700

    update

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,12 +11,9 @@ int _pow_recursion(int x, int y)
 {
     if (y < 0)
         return (-1); /* Error: negative exponent */
-
     if (y == 0)
         return (1); /* x^0 is 1 */
-
     if (y == 1)
         return (x); /* x^1 is x */
-
     return (x * _pow_recursion(x, y - 1)); /* Recursive call to calculate x^y */
 }
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,38 +1,36 @@
 #include"main.h"
 
 /**
- * _sqrt_recursion - function that calculate  the natural square root of a number.
- *
- * @n: the integer we want to calculate it square.
- *
- * Return: the result of the natural square.
- */
-
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-        	return (-1);  /* Square root is not defined for negative numbers */
-
-    	if (n == 0 || n == 1)
-        	return (n);  /* Base cases: sqrt(0) = 0 and sqrt(1) = 1 */
-
-    	return (_sqrt_helper(n, 1));
-}
-
-/**
  * _sqrt_helper - Helper function to find the square root using recursion.
- * @n: The integer for which to calculate the square root.
+ * @n: The number for which to calculate the square root.
  * @guess: The current guess for the square root.
  *
- * Return: The integer square root of the number, or -1 if it's not an integer square.
+ * Return: The natural square root of the number, or -1 if it's not a perfect square.
  */
 int _sqrt_helper(int n, int guess)
 {
     if (guess * guess == n)
-        return (guess);  /* Found the square root */
+        return guess; /* Found the square root */
 
-    if (guess * guess > n)
-        return (-1);  /* n is not a perfect square, return -1 */
+    if (guess <= 0)
+        return -1; /* The number is not a perfect square, return -1 */
 
-    return (_sqrt_helper(n, guess + 1)); /* Recursively increment the guess */
+    return _sqrt_helper(n, guess - 1);
+}
+
+/**
+ * _sqrt_recursion - Calculates the natural square root of a number.
+ * @n: The number for which to calculate the square root.
+ *
+ * Return: The natural square root of the number, or -1 if it's not a perfect square.
+ */
+int _sqrt_recursion(int n)
+{
+    if (n < 0)
+        return -1; /* Square root is not defined for negative numbers */
+
+    if (n == 0 || n == 1)
+        return n; /* Base cases: sqrt(0) = 0 and sqrt(1) = 1 */
+
+    return _sqrt_helper(n, n / 2);
 }
