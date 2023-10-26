e1dffc11f85a7fe08da31646ed322ef23c48ccf0
EL-IDRISSI-Imad <iei.2002.imad@gmail.com>
2023-10-26 02:05:59 -0700

    update

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,18 +2,19 @@
 
 /**
  * _pow_recursion - Calculates x raised to the power of y.
+ *
  * @x: The base number.
  * @y: The exponent.
- *
  * Return: The result of x^y.
  */
+
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
-        return (-1); /* Error: negative exponent */
-    if (y == 0)
-        return (1); /* x^0 is 1 */
-    if (y == 1)
-        return (x); /* x^1 is x */
-    return (x * _pow_recursion(x, y - 1)); /* Recursive call to calculate x^y */
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (y == 1)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
 }
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,32 +5,30 @@
  * @n: The number for which to calculate the square root.
  * @guess: The current guess for the square root.
  *
- * Return: The natural square root of the number, or -1 if it's not a perfect square.
+ * Return: The natural square root of the number, or -1 otherwise
  */
+
 int _sqrt_helper(int n, int guess)
 {
-    if (guess * guess == n)
-        return guess; /* Found the square root */
-
-    if (guess <= 0)
-        return -1; /* The number is not a perfect square, return -1 */
-
-    return _sqrt_helper(n, guess - 1);
+	if (guess * guess == n)
+		return guess; /* Found the square root */
+	if (guess <= 0)
+		return -1; /* The number is not a perfect square, return -1 */
+	return (_sqrt_helper(n, guess - 1));
 }
 
 /**
  * _sqrt_recursion - Calculates the natural square root of a number.
  * @n: The number for which to calculate the square root.
  *
- * Return: The natural square root of the number, or -1 if it's not a perfect square.
+ * Return: The natural square root of the number, or -1 otherwise.
  */
+
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return -1; /* Square root is not defined for negative numbers */
-
-    if (n == 0 || n == 1)
-        return n; /* Base cases: sqrt(0) = 0 and sqrt(1) = 1 */
-
-    return _sqrt_helper(n, n / 2);
+	if (n < 0)
+		return (-1); /* Square root is not defined for negative numbers */
+	if (n == 0 || n == 1)
+		return (n); /* Base cases: sqrt(0) = 0 and sqrt(1) = 1 */
+	return (_sqrt_helper(n, n / 2));
 }
