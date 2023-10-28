b5f3a13cf622c0c1b234bf0f3c3d65d06ee70124
EL-IDRISSI-Imad <iei.2002.imad@gmail.com>
2023-10-28 06:57:04 -0700

    updated

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - Multiplies two numbers.
- * @argc: Number of command-line arguments.
- * @argv: Array of command-line argument strings.
- * Return: 0 for success, 1 for error.
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
@@ -15,11 +13,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int result = num1 * num2;
-
-	printf("%d\n", result);
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,54 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
 /**
- * is_positive_number - Check if a string is a positive number
- * @str: The string to check
- * Return: 1 if it's a positive number, 0 otherwise
- */
-int is_positive_number(const char *str)
-{
-	if (str == NULL || *str == '\0')
-		return (0);
-
-	for (int i = 0; str[i] != '\0'; i++)
-	{
-		if (!isdigit(str[i]))
-			return (0);
-	}
-	return (1);
-}
-
-/**
- * main - Adds positive numbers.
- * @argc: Number of command-line arguments.
- * @argv: Array of command-line argument strings.
- * Return: 0 for success, 1 for error.
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
-	int sum = 0;
-	
-	if (argc == 1)
-	{
-		printf("0\n");
-		return (0);
-	}
+	int i, j, add = 0;
 
-	for (int i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
-		if (is_positive_number(argv[i]))
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			sum += atoi(argv[i]);
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
+		add += atoi(argv[i]);
 	}
-
-	printf("%d\n", sum);
+	printf("%d\n", add);
 	return (0);
 }
