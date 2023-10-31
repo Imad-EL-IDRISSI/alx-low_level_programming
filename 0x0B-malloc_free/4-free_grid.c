08ba99b08e3666e180cc318bf6e6136eb80684b5
EL-IDRISSI-Imad <iei.2002.imad@gmail.com>
2023-10-30 17:47:31 -0700

    updated

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -7,10 +7,11 @@
  */
 void free_grid(int **grid, int height)
 {
-	if (grid == NULL)
-		return;
+	int i;
+
+	if (grid == NULL) return;
 	
-	for (int i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
     {
         if (grid[i] != NULL)
             free(grid[i]);
