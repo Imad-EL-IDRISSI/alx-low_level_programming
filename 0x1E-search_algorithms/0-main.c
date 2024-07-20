f0b0431eeaa29e33156bbb8f6cd3988a3b71ccb0
EL-IDRISSI-Imad <iei.2002.imad@gmail.com>
2024-07-20 09:55:38 -0700

    0-linear

---
+++ 0x1E-search_algorithms/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "search_algos.h"
+
+int main(void)
+{
+	    int array[] = {10, 20, 30, 40, 50};
+	        size_t size = sizeof(array) / sizeof(array[0]);
+		    int value = 30;
+		        int index;
+
+			    index = linear_search(array, size, value);
+			        printf("Value %d found at index %d\n", value, index);
+
+				    return 0;
+}
